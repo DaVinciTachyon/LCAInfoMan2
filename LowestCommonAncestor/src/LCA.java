05ec7e5609fe21208dcd1947c0474c9a2af41f0b
DaVinciTachyon <davincitachyon@gmail.com>
2019-10-03 20:50:25 +0100

    wrote initial tests for Node class

--- LowestCommonAncestor/src/LCA.java
+++ LowestCommonAncestor/src/LCA.java
@@ -1,40 +1,5 @@
 public class LCA {
+
     LCA() {
-
-    }
-
-    class Node {
-        int data;
-        Node left, right; //make arraylist
-
-        Node(int value) {
-            setData(value);
-            setLeft(null);
-            setRight(null);
-        }
-
-        public int getData() {
-            return data;
-        }
-
-        public void setData(int data) {
-            this.data = data;
-        }
-
-        public Node getLeft() {
-            return left;
-        }
-
-        public void setLeft(Node left) {
-            this.left = left;
-        }
-
-        public Node getRight() {
-            return right;
-        }
-
-        public void setRight(Node right) {
-            this.right = right;
-        }
     }
 }
--- LowestCommonAncestor/src/LCATest.java
+++ LowestCommonAncestor/src/LCATest.java
@@ -1,5 +1,40 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class LCATest {
-
+@RunWith(JUnit4.class)
+public class LCATest {
+    @Test
+    public void testNode() {
+        Node root = new Node(1);
+        assertEquals(1, root.getData());
+        assertNull(root.getLeft());
+        assertNull(root.getRight());
+        root.setLeft(new Node(2));
+        assertEquals(2, root.getLeft().getData());
+        assertNull(root.getLeft().getLeft());
+        assertNull(root.getLeft().getRight());
+        root.setRight(new Node(3));
+        assertEquals(3, root.getRight().getData());
+        assertNull(root.getRight().getLeft());
+        assertNull(root.getRight().getRight());
+        root.getLeft().setLeft(new Node(4));
+        assertEquals(4, root.getLeft().getLeft().getData());
+        assertNull(root.getLeft().getLeft().getLeft());
+        assertNull(root.getLeft().getLeft().getRight());
+        root.getLeft().setRight(new Node(5));
+        assertEquals(5, root.getLeft().getRight().getData());
+        assertNull(root.getLeft().getRight().getLeft());
+        assertNull(root.getLeft().getRight().getRight());
+        root.getRight().setLeft(new Node(6));
+        assertEquals(6, root.getRight().getLeft().getData());
+        assertNull(root.getRight().getLeft().getLeft());
+        assertNull(root.getRight().getLeft().getRight());
+        root.getRight().setRight(new Node(7));
+        assertEquals(7, root.getRight().getRight().getData());
+        assertNull(root.getRight().getRight().getLeft());
+        assertNull(root.getRight().getRight().getRight());
+    }
 }
